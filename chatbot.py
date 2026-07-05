1cf81df2abf371aeec35ff1f3fc79519f5c4c679
Kirti Pandey <kirtipandz18@gmail.com>
2026-07-05 15:08:26 +0530

    describe what you changed

--- chatbot.py
+++ chatbot.py
@@ -26,7 +26,7 @@ load_dotenv("details.env")
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash",
+    model="gemini-2.5-flash",
     contents="Say hello in one word"
 )
 print(response.text)
